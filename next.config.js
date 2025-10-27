4c440e325d4d4adb822669dc117c900e467b198d
atshybrid <amoghnyatechsolutions@gmail.com>
2025-10-27 23:23:00 +0530

    chore(dev): transpile react-simple-maps/d3 to improve HMR; minor dev UX tweaks to reduce full reloads

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Transpile certain ESM packages to improve HMR/Fast Refresh handling in dev
+  transpilePackages: ['react-simple-maps', 'd3', 'topojson-client'],
 }
 
 module.exports = nextConfig
